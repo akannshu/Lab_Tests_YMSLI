8e78fb6fb4702823cbe605fb9285404d8e6a8c84
Akanshu Raj <VE00YM232@YMI.COM>
2021-03-08 10:05:52 +0530

    springjdbc completed

--- traineeapp/src/com/traineeapp/dao/user/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.traineeapp.dao.user;
-
-public interface UserDao {
-	public void addUser(User user);
-
-	public User getUser(String username, String password);
-}
--- traineeapp/src/com/traineeapp/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.traineeapp.service;
-
-import com.traineeapp.dao.user.User;
-
-public interface UserService {
-	public void addUser(User user);
-	public User getUser(String username, String password);
-}
